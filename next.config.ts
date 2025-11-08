afcf6ad3c0a8a46c6203e43648d60ade3e306b62
Hoi Fung Alan Tsang <thfalan@gmail.com>
2025-11-08 22:26:13 +0800

    maintain url

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://www.techgrity.info/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export function middleware() {
-  return NextResponse.redirect('https://www.techgrity.info')
-}
-
-export const config = {
-  matcher: '/:path*',
-} 

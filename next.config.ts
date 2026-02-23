ddd6ba561ffbcf811620c9582c19a3dcfc2ed73c
thfalan <thfalan@gmail.com>
2026-02-23 18:40:14 +0800

    add simbank redirection

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
+  async redirects() {
     return [
       {
         source: "/:path*",
-        destination: "https://docs.liquidroyalty.com/:path*",
+        destination: "https://simbank.com/:path*",
+        permanent: true,
       },
     ];
   },
